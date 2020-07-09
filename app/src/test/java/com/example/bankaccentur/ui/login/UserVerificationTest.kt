ea05a6f30194b98c9abc04b9caf36fbe3543af67
Bruno Cardoso <brunowcnascimento@gmail.com>
2020-07-09 18:46:31 -0300

    Teste do CPF realizado os testes de CPF

--- app/src/test/java/com/example/bankaccentur/ui/login/UserVerificationTest.kt
+++ app/src/test/java/com/example/bankaccentur/ui/login/UserVerificationTest.kt
@@ -406,5 +406,65 @@ class UserVerificationTest {
         }
     }
 
+    class verifyCpf {
+
+        val verify = UserVerification()
+
+        @Test
+        fun verifyCpf_InvalidCpf_Inserting_Name() {
+            val cpf = "teste"
+            val validateCpf = verify.verifyCpf(cpf)
+            assertEquals(false, validateCpf)
+        }
+
+        @Test
+        fun verifyCpf_InvalidCpf_Inserting_SpecialCharacter() {
+            val cpf = "@@@@@"
+            val validateCpf = verify.verifyCpf(cpf)
+            assertEquals(false, validateCpf)
+        }
+
+        @Test
+        fun verifyCpf_InvalidCpf_Inserting_IncompleteCpf_10Numbers() {
+            val cpf = "2534148362"
+            val validateCpf = verify.verifyCpf(cpf)
+            assertEquals(false, validateCpf)
+        }
+
+        @Test
+        fun verifyCpf_InvalidCpf_Inserting_Name_CompletCpf() {
+            val cpf = "Teste25341483626"
+            val validateCpf = verify.verifyCpf(cpf)
+            assertEquals(false, validateCpf)
+        }
+
+        @Test
+        fun verifyCpf_InvalidCpf_Inserting_More_Than_11_Numbers() {
+            val cpf = "253414836261"
+            val validateCpf = verify.verifyCpf(cpf)
+            assertEquals(false, validateCpf)
+        }
+
+        @Test
+        fun verifyCpf_InvalidCpf_Inserting_Vazio() {
+            val cpf = ""
+            val validateCpf = verify.verifyCpf(cpf)
+            assertEquals(false, validateCpf)
+        }
+
+        @Test
+        fun verifyCpf_CorrectCpf_Inserting_Simple_Cpf(){
+            val cpf = "92526376483"
+            val validateCpf = verify.verifyCpf(cpf)
+            assertEquals(true, validateCpf)
+
+            val OtherCpf = "25341483626"
+            val validateOtherCpf = verify.verifyCpf(OtherCpf)
+            assertEquals(true, validateOtherCpf)
+        }
+
+
+
+    }
 
 }
